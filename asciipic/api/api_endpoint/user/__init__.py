63e1de28201a1442855f9604508ffa1fa6377fa8
Matei-Marius Micu <micumatei@gmail.com>
2017-04-30 00:35:25 +0300

    Add User Endpoint

--- asciipic/api/api_endpoint/user/__init__.py
+++ asciipic/api/api_endpoint/user/__init__.py
@@ -1,19 +1,16 @@
-"""API  endpoint for the AsciiPic API."""
+"""User EndPoint for AsciiPic."""
 
 from asciipic.api import base as base_api
 from asciipic.api.api_endpoint import version
-from asciipic.api.api_endpoint import user
 
 
-class APIEndpoint(base_api.BaseAPI):
+class UserEndpoint(base_api.BaseAPI):
 
-    """This API will have all the API endpoints."""
+    """User EndPoint for AsciiPic."""
 
     # A list that contains all the resources (endpoints) available for the
     # current metadata service
     resources = [
-        ("version", version.Version),
-        ("user", user.UserEndpoint)
     ]
 
     # Whether this application should be available for clients
