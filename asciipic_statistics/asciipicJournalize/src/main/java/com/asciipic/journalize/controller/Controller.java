952aa69d9c651d0f253a148baa006f65828089b8
AlexandraFolvaiter <folvaiter.alexandra@gmail.com>
2017-05-08 18:31:52 +0300

    Basic structure for journalize API

---
+++ asciipic_statistics/asciipicJournalize/src/main/java/com/asciipic/journalize/controller/Controller.java
@@ -0,0 +1,10 @@
+package com.asciipic.journalize.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/v1/statistics")
+public class Controller {
+
+
+}
