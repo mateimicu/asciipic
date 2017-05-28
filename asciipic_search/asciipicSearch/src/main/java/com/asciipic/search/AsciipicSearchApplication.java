c1f2d3d6e84124e23b0593f70dca79c3ac0672e3
AlexandraFolvaiter <folvaiter.alexandra@gmail.com>
2017-05-28 16:09:50 +0300

    Basic api for search

---
+++ asciipic_search/asciipicSearch/src/main/java/com/asciipic/search/AsciipicSearchApplication.java
@@ -0,0 +1,12 @@
+package com.asciipic.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsciipicSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsciipicSearchApplication.class, args);
+	}
+}
