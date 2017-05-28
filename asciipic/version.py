d3e172e2ec914b325aff271a60618b9f74c59514
Matei-Marius Micu <micumatei@gmail.com>
2017-05-28 16:13:13 +0300

    Fix pbr version problem
    Because Heroku is not installing our app we can't acces the
    verson using pbr. This is a cheep workaround that works for now.

--- asciipic/version.py
+++ asciipic/version.py
@@ -1,8 +1,13 @@
 """Version info."""
 import pbr.version
+HARD_VERSION = "0.0.1"
 
 
 def get_version():
     """Obtain the project version."""
-    version = pbr.version.VersionInfo('asciipic')
-    return version.release_string()
+    try:
+        version = pbr.version.VersionInfo('asciipic')
+        return version.release_string()
+    # pylint:disable=broad-except
+    except Exception:
+        return HARD_VERSION
